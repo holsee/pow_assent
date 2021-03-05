d16c8446d6f01de994bab3e557789c2e16b71883
Steven Holdsworth <sholdsworth@gmail.com>
2021-03-05 18:29:44 +0000

    Depend on holsee/pow fix/remove_reload_after_write

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule PowAssent.MixProject do
 
   defp deps do
     [
-      {:pow, "~> 1.0.19"},
+      {:pow, github: "holsee/pow", branch: "fix/remove_reload_after_write"},
       {:assent, "~> 0.1.2"},
 
       {:ecto, "~> 2.2 or ~> 3.0"},
